315ae6b78f203db6697f7d99a2baa8ae4c6f673b
Stefano Sonzogni <sonzo995@gmail.com>
2024-10-06 10:44:34 +0200

    implement executable to run server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"stefano.sonzogni/tic-tac-toe/internal/server"
+)
 
 func main() {
-	fmt.Println("Hello from server")
+	matchmaker := server.WsMatchmaker{}
+
+	mux := http.NewServeMux()
+
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	mux.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
+		slog.Debug("received play request")
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		slog.Debug("connection established")
+		matchmaker.Enqueue(conn)
+	})
+
+	err := http.ListenAndServe(":5001", mux)
+	if err != nil {
+		slog.Error("failed to start server", "err", err.Error())
+	}
 }
